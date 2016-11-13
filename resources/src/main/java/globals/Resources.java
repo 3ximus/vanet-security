b1265b96c3163edc9f8d6cc9aac6e42fcbc406f5
eximus <fabio_r11@hotmail.com>
2016-11-13 16:05:15 +0000

    microscopic tweak for branch coherence

--- resources/src/main/java/globals/Resources.java
+++ resources/src/main/java/globals/Resources.java
@@ -1,8 +1,9 @@
 package globals;
 
 public class Resources {
-    public static final int CA_PORT_NUMBER = 10000;
-    public static final int RSU_PORT_NUMBER = 11000;
+    public static final int CA_PORT = 10000;
+    public static final String CA_NAME = "CA";
+    public static final int RSU_PORT = 11000;
 
     public static String ERROR_MSG(String msg) { return "[\033[0;31m] ERROR \033[0m]]"+ msg; }
     public static String WARNING_MSG(String msg) { return "[\033[0;33m]WARNING\033[0m]]" + msg; }
