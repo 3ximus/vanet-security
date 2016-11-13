6bc46f7127d0dd8a560941b1e9813754b7250f7f
Andre Sobral <andre.sobral@tecnico.ulisboa.pt>
2016-11-13 18:01:00 +0000

    CA compiling and running

--- ca/src/main/java/security/CAApp.java
+++ ca/src/main/java/security/CAApp.java
@@ -1,18 +1,19 @@
 package security;
 
-import globals.*;
-import java.rmi.*;
+import globals.Resources; 
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 
 public class CAApp {
     public static void main( String[] args ) {
         try {
             RemoteCAService caService = new RemoteCAService();
-            Registry reg = LocateRegistry.createRegistry(CA_PORT);
-            reg.rebind(CA_NAME, caService);
+            Registry reg = LocateRegistry.createRegistry(Resources.CA_PORT);
+            reg.rebind(Resources.CA_NAME, caService);
 
-            System.out.println(OK_MSG("CA Server Online."));
+            System.out.println(Resources.OK_MSG("CA Server Online."));
         } catch ( Exception e ) {
-            System.out.println(ERROR_MSG("Launching CA Server: " + e.getMessage()));
+            System.out.println(Resources.ERROR_MSG("Launching CA Server: " + e.getMessage()));
         }
     }
 }
