d4dda7cea4d0c64e91c38040640b5443118537e5
Andre Sobral <andre.sobral@tecnico.ulisboa.pt>
2016-11-14 10:08:33 +0000

    rsu main done

--- rsu/src/main/java/gateway/RSUApp.java
+++ rsu/src/main/java/gateway/RSUApp.java
@@ -8,15 +8,15 @@ import java.rmi.server.UnicastRemoteObject;
 import globals.Resources;
 import remote.RemoteCAInterface;
 import remote.RemoteVehicleNetworkInterface;
-import gateway.RemoteRSUService;
+import remote.RemoteRSUInterface;
 
 
-public class App
+public class RSUApp
 {
 	// publishes RSU service to RMI
 	public static void publishRSU(RemoteRSUService rsu_service) {
 		try {
-            RemoteRSUService stub = (RemoteRSUService) UnicastRemoteObject.exportObject(rsu_service, 0);
+            RemoteRSUInterface stub = (RemoteRSUInterface) UnicastRemoteObject.exportObject(rsu_service, 0);
             Registry registry = LocateRegistry.getRegistry();
             registry.rebind(Resources.RSU_NAME , stub);
             System.out.println(Resources.OK_MSG("RemoteRSUService bound"));
@@ -28,7 +28,7 @@ public class App
 	}
 
     public static void main( String[] args ) {
-        
+
     	RSU rsu = new RSU();
         RemoteVehicleNetworkInterface vehicle_service = null;
         RemoteCAInterface ca_service = null;
@@ -38,21 +38,24 @@ public class App
             Registry registry = LocateRegistry.getRegistry(Resources.REGISTRY_PORT); 
             vehicle_service
             		= (RemoteVehicleNetworkInterface) registry.lookup(Resources.VANET_NAME);
+            System.out.println(Resources.OK_MSG("Remote Vehicle Network Interface located"));
 
             // Locate the certificate authority service
             registry = LocateRegistry.getRegistry(Resources.REGISTRY_PORT);
             ca_service
             		= (RemoteCAInterface) registry.lookup(Resources.CA_NAME);
+            System.out.println(Resources.OK_MSG("Remote Certificate Authority Interface located"));
+
+            // Constroi objeto remoto
+            RemoteRSUService rsu_service = new RemoteRSUService(rsu,ca_service,vehicle_service);
+
+            // Publica-o no RMI registry
+            publishRSU(rsu_service);
 
         } catch (Exception e) {
-            System.err.println("Remote lookup exception:");
+            System.err.println(Resources.ERROR_MSG("Remote lookup exception:"));
             e.printStackTrace();
         }
 
-    	// Constroi objeto remoto
-    	RemoteRSUService rsu_service = new RemoteRSUService(rsu,ca_service,vehicle_service);
-
-        // Publica-o no RMI registry
-    	publishRSU(rsu_service);
     }
 }
