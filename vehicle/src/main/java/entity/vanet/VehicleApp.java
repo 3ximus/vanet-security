abe7f346bfd27854a0658c7dc3970477a0d634a9
eximus <fabio_r11@hotmail.com>
2016-11-15 05:31:35 +0000

    fixed tabs

--- vehicle/src/main/java/entity/vanet/VehicleApp.java
+++ vehicle/src/main/java/entity/vanet/VehicleApp.java
@@ -13,90 +13,90 @@ import java.rmi.registry.Registry;
  *
  */
 public class VehicleApp {
-    public static final String VEHICLE_ARGS_USAGE = "vehicle <VIN> <posX> <posY> <velX> <velY>";
+	public static final String VEHICLE_ARGS_USAGE = "vehicle <VIN> <posX> <posY> <velX> <velY>";
 
-    public static void main(String[] args) {
-        System.out.println("\n");
+	public static void main(String[] args) {
+		System.out.println("\n");
 
-        Vehicle vehicle;
+		Vehicle vehicle;
 
-        // Parse args
-        if(args.length == 0) {
-            System.out.println(Resources.NOTIFY_MSG("Assuming random values for position, velocity and VIN."));
-            System.out.println(Resources.NOTIFY_MSG("To specify this values you could call with: " + VEHICLE_ARGS_USAGE + "."));
+		// Parse args
+		if(args.length == 0) {
+			System.out.println(Resources.NOTIFY_MSG("Assuming random values for position, velocity and VIN."));
+			System.out.println(Resources.NOTIFY_MSG("To specify this values you could call with: " + VEHICLE_ARGS_USAGE + "."));
 // FIX ASAP !!!
-            vehicle = new Vehicle("VIN1", "vehicle1", new Vector2Df(0, 0), new Vector2Df(0, 0)); // < --- FIXME.. PLS
+			vehicle = new Vehicle("VIN1", "vehicle1", new Vector2Df(0, 0), new Vector2Df(0, 0)); // < --- FIXME.. PLS
 // FIX ASAP !!!
 
-        } else if(args.length == 6) {
-            try {
-                float px = Float.parseFloat(args[2]);
-                float py = Float.parseFloat(args[3]);
-                float vx = Float.parseFloat(args[4]);
-                float vy = Float.parseFloat(args[5]);
-                vehicle = new Vehicle(args[0], args[1], new Vector2Df(px, py), new Vector2Df(vx, vy));
-            } catch (NumberFormatException e) {
-                System.out.println(Resources.ERROR_MSG("[Vehicle]Received the correct amount of arguments but couldn't convert to float."));
-                return;
-            }
-        } else {
-            System.out.println(Resources.ERROR_MSG("[Vehicle] Incorrect amount of arguments."));
-            System.out.println("[Vehicle] Usage: " + VEHICLE_ARGS_USAGE);
-            return;
-        }
-        System.out.println("[Vehicle] Started: " + vehicle);
+		} else if(args.length == 6) {
+			try {
+				float px = Float.parseFloat(args[2]);
+				float py = Float.parseFloat(args[3]);
+				float vx = Float.parseFloat(args[4]);
+				float vy = Float.parseFloat(args[5]);
+				vehicle = new Vehicle(args[0], args[1], new Vector2Df(px, py), new Vector2Df(vx, vy));
+			} catch (NumberFormatException e) {
+				System.out.println(Resources.ERROR_MSG("[Vehicle]Received the correct amount of arguments but couldn't convert to float."));
+				return;
+			}
+		} else {
+			System.out.println(Resources.ERROR_MSG("[Vehicle] Incorrect amount of arguments."));
+			System.out.println("[Vehicle] Usage: " + VEHICLE_ARGS_USAGE);
+			return;
+		}
+		System.out.println("[Vehicle] Started: " + vehicle);
 
-        // Create registry if it doesn't exist
-        try {
-            LocateRegistry.createRegistry(Resources.REGISTRY_PORT);
-        } catch(Exception e) {
-            // registry is already created
-        }
+		// Create registry if it doesn't exist
+		try {
+			LocateRegistry.createRegistry(Resources.REGISTRY_PORT);
+		} catch(Exception e) {
+			// registry is already created
+		}
 
-        // Connect to the VANET
-        RemoteVehicleNetworkInterface VANET;
-        String vehicleUniqueName;
-        try {
-            Registry registry = LocateRegistry.getRegistry(Resources.REGISTRY_PORT);
-            VANET = (RemoteVehicleNetworkInterface) registry.lookup(Resources.VANET_NAME);
-            // Get a unique name from the VANET
-            vehicleUniqueName = VANET.getNextVehicleName();
-        } catch(Exception e) {
-            System.err.println(Resources.ERROR_MSG("[Vehicle] Failed to connect to VANET. " + e.getClass() + ": " +  e.getMessage()));
-            System.exit(0); // Return seems to not work for some reason
-            return;
-        }
+		// Connect to the VANET
+		RemoteVehicleNetworkInterface VANET;
+		String vehicleUniqueName;
+		try {
+			Registry registry = LocateRegistry.getRegistry(Resources.REGISTRY_PORT);
+			VANET = (RemoteVehicleNetworkInterface) registry.lookup(Resources.VANET_NAME);
+			// Get a unique name from the VANET
+			vehicleUniqueName = VANET.getNextVehicleName();
+		} catch(Exception e) {
+			System.err.println(Resources.ERROR_MSG("[Vehicle] Failed to connect to VANET. " + e.getClass() + ": " +  e.getMessage()));
+			System.exit(0); // Return seems to not work for some reason
+			return;
+		}
 
-        // Publish remote vehicle
-        RemoteVehicleService remoteVehicle = new RemoteVehicleService(vehicle, vehicleUniqueName);
-        remoteVehicle.publish();
+		// Publish remote vehicle
+		RemoteVehicleService remoteVehicle = new RemoteVehicleService(vehicle, vehicleUniqueName);
+		remoteVehicle.publish();
 
-        // Start the vehicle
-        vehicle.start(VANET, vehicleUniqueName);
+		// Start the vehicle
+		vehicle.start(VANET, vehicleUniqueName);
 
-        // Add vehicle to the VANET
-        try {
-            boolean result = VANET.addVehicle(vehicleUniqueName);
-            if(result == false) {
-                throw new Exception("Remote call to the VANET to add this vehicle failed.");
-            }
-        } catch(Exception e) {
-            System.err.println(Resources.ERROR_MSG("[Vehicle] Failed add vehicle to the VANET. " + e.getClass() + ": " +  e.getMessage()));
-            System.exit(0); // Return seems to not work for some reason
-            return;
-        }
+		// Add vehicle to the VANET
+		try {
+			boolean result = VANET.addVehicle(vehicleUniqueName);
+			if(result == false) {
+				throw new Exception("Remote call to the VANET to add this vehicle failed.");
+			}
+		} catch(Exception e) {
+			System.err.println(Resources.ERROR_MSG("[Vehicle] Failed add vehicle to the VANET. " + e.getClass() + ": " +  e.getMessage()));
+			System.exit(0); // Return seems to not work for some reason
+			return;
+		}
 
-        // Handle wait and removal
-        try {
-            System.out.println("[Vehicle] Press <enter> to kill the vehicle.");
-            System.in.read();
-            VANET.removeVehicle(vehicleUniqueName);
-        } catch (java.io.IOException e) {
-            System.out.println(Resources.ERROR_MSG("[Vehicle] Unable to read from input. Exiting."));
-        } finally {
-            remoteVehicle.unpublish();
-            System.out.println("\n");
-            System.exit(0);
-        }
-    }
+		// Handle wait and removal
+		try {
+			System.out.println("[Vehicle] Press <enter> to kill the vehicle.");
+			System.in.read();
+			VANET.removeVehicle(vehicleUniqueName);
+		} catch (java.io.IOException e) {
+			System.out.println(Resources.ERROR_MSG("[Vehicle] Unable to read from input. Exiting."));
+		} finally {
+			remoteVehicle.unpublish();
+			System.out.println("\n");
+			System.exit(0);
+		}
+	}
 }
