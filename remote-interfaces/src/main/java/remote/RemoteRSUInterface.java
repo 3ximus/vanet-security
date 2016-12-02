e0dccd259af950c671bc32e14f6746e00e734378
eximus <fabio_r11@hotmail.com>
2016-12-02 14:29:06 +0000

    RSU interface changes

--- remote-interfaces/src/main/java/remote/RemoteRSUInterface.java
+++ remote-interfaces/src/main/java/remote/RemoteRSUInterface.java
@@ -2,42 +2,40 @@ package remote;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-import java.security.cert.Certificate;
+
+import globals.SignedCertificateDTO;
 
 
 public interface RemoteRSUInterface extends Remote {
 
 	/**
 	 * Verifies revoked state of a given certificate ( uses RSU cache, if not found asks the CA )
-	 * @param	Certificate		certificate to check validity
-	 * @param	Certificate		senders certificate to verify message signature
-	 * @param	byte[]			message signature
-	 * @return	boolean			true if its valid, false if its revoked
+	 * @param	SignedCertificateDTO	DTO containing certificate to be checked
+	 * @return		 if its valid, false if its revoked
 	 */
-	public boolean isRevoked(Certificate certToVerify, Certificate senderCert, byte[] signature) throws RemoteException;
-	//public boolean isRevoked(AuthenticatedCertificateDTO message) throws RemoteException;
+	public boolean isRevoked(SignedCertificateDTO dto) throws RemoteException;
 
 
 	/**
 	 * Ask RSU to revoke certificate, reroutes request to CA.
 	 * CA will handle certificate revocation, RSU will only maintain a cache if result
 	 * of revocation is true.
-	 * @param	Certificate		reported target certificate to be revoked
-	 * @param	Certificate		author of the report
-	 * @param	byte[]			message signature
+	 * @param	SignedCertificateDTO	DTO containing certificate to be revoked
 	 */
-	public boolean tryRevoke(Certificate certToRevoke, Certificate senderCertificate, byte[] signature) throws RemoteException;
-	//public boolean tryRevoke(AuthenticatedCertificateDTO message) throws RemoteException;
-
-	public void shareRevoked(/* TODO add argumensts */) throws RemoteException;
-
-
-	public void enforceRevocation(/* TODO add argumensts */) throws RemoteException;
+	public boolean tryRevoke(SignedCertificateDTO dto) throws RemoteException;
 
 	/**
-	 * Connectivity test
-	 * @param	String	message to ping;
-	 * @return	String	pong
+	 * Share a new revoked certificate with RSU's in the vicinity
+	 * Simulates a sort of ranged broadcast through the network
+	 * @param	SignedCertificateDTO	DTO containing certificate to be shared
 	 */
-	public String ping(String msg) throws RemoteException;
+	public void shareRevoked(SignedCertificateDTO dto) throws RemoteException;
+
+
+	/**
+	 * Propagate new revoked certificates to vehicles in range
+	 * @param	SignedCertificateDTO	DTO containing certificate to be shared
+	 */
+	public void informVehiclesOfRevocation(SignedCertificateDTO dto) throws RemoteException;
+
 }
