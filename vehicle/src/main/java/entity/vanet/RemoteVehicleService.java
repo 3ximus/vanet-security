f415be1eba0087dbd7381ad1254a670dddd23fc8
Vasco-jofra <joaovascofranco@hotmail.com>
2016-12-05 16:51:33 +0000

    new profile

--- vehicle/src/main/java/entity/vanet/RemoteVehicleService.java
+++ vehicle/src/main/java/entity/vanet/RemoteVehicleService.java
@@ -93,9 +93,9 @@ public class RemoteVehicleService implements RemoteVehicleInterface {
 		if (!dto.verifyCertificate(this.vehicle.getCACertificate())) {
 			System.out.println(Resources.WARNING_MSG("Invalid CA Signature on beacon: " + dto.toString()));
 
-			SignedCertificateDTO my_dto 
-					= new SignedCertificateDTO(dto.getSenderCertificate(), 
-											   this.vehicle.getCertificate(), 
+			SignedCertificateDTO my_dto
+					= new SignedCertificateDTO(dto.getSenderCertificate(),
+											   this.vehicle.getCertificate(),
 											   this.vehicle.getPrivateKey());
 			vehicle.reportCertificate(my_dto);
 			return false;  // certificate was not signed by CA, beacon is dropped
