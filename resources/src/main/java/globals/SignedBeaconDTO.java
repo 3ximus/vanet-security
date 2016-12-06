0573a4f613bf740a9dfbe632f23785a82b85fd54
3ximus <fabio_r11@hotmail.com>
2016-12-06 14:43:10 +0000

    changed dto sign methods to superclass

--- resources/src/main/java/globals/SignedBeaconDTO.java
+++ resources/src/main/java/globals/SignedBeaconDTO.java
@@ -57,47 +57,5 @@ public class SignedBeaconDTO extends SignedDTO {
 		System.arraycopy(serializedCert, 0, newSerialization, serializedDTO.length, serializedCert.length);
 		return newSerialization;
 	}
-
-	/**
-	 * Generates the signature for the DTO + senderCertificate in the constructor with given PrivateKey
-	 * @param	PrivateKey	key used to signed this DTO
-	 * @return	returns the signature generated
-	 *
-	 * <p><b>NOTE:</b> This also sets the internal atribute <b>signature</b> that can be acessed with <b>getSignature</b></p>
-	 */
-	@Override
-	public byte[] sign(PrivateKey pKey) {
-		byte[] serializedVal = this.serialize();
-		byte[] sig = null;
-		try {
-			sig = Resources.makeDigitalSignature(serializedVal, pKey); }
-		catch (Exception e) {
-			System.out.println(Resources.ERROR_MSG("Failed to create signature: "+e.getMessage()));
-			return null;
-		}
-		this.signature = sig;
-		return this.signature;
-	}
-
-	/*
-	 * Verify if the senderCertificate was signed by the given entity
-	 * @param	X509Certificate	entity used to verify the senderCertificate in this DTO (usually the CA)
-	 * @return	returns true if it was signed by given entity
-	 */
-	@Override
-	public boolean verifyCertificate(X509Certificate otherEntity) {
-		return Resources.verifySignedCertificate(this.senderCertificate, otherEntity.getPublicKey());
-	}
-
-	/**
-	 * Compares the received signature with the calculated DTO signature
-	 * @return	returns true if the signature is correct
-	 */
-	@Override
-	public boolean verifySignature() {
-		try { Resources.verifyDigitalSignature(this.signature, this.serialize(), this.senderCertificate.getPublicKey()); }
-		catch (Exception e ) { return false; } // certificate was not signed by sender
-		return true;
-	}
 }
 
