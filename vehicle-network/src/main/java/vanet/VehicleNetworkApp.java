01580680f95391b8a2edfd3f555fb53eb1c8592f
Vasco-jofra <joaovascofranco@hotmail.com>
2016-12-08 11:13:11 +0000

    I fucking did it. GUI in vehicle network

--- vehicle-network/src/main/java/vanet/VehicleNetworkApp.java
+++ vehicle-network/src/main/java/vanet/VehicleNetworkApp.java
@@ -4,6 +4,9 @@ import globals.Resources;
 
 import java.rmi.registry.LocateRegistry;
 
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 public class VehicleNetworkApp {
     public static void main(String[] args) {
         System.out.println("\n");
@@ -19,6 +22,11 @@ public class VehicleNetworkApp {
         RemoteVehicleNetworkService VANET = new RemoteVehicleNetworkService(vehicleNetwork);
         VANET.publish();
 
+        // Launch GUI
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        new LwjglApplication(new vanet.gui.VanetGUI(), config);
+
+        // Handle wait and unpublish
         try {
             System.out.println("Press enter to kill the network.");
             System.in.read();
